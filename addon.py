bcd6cadb94e8db537583bb7a98204003f8fbf384
harddevelop <hard.develop@gmail.com>
2015-11-21 17:20:59 +0100

    Added new provider live9.net

--- addon.py
+++ addon.py
@@ -15,6 +15,7 @@ from providers.vigoalnet import Vigoal
 from providers.cinestrenostv import Cineestrenostv
 from providers.cricfreetv import Cricfreetv
 from providers.zoptvcom import Zoptvcom
+from providers.live9net import Live9net
 from core.decoder import Decoder
 import re
 
@@ -69,7 +70,7 @@ def add_dir(name,url,mode,iconimage,provider,page="", thumbnailImage=''):
 	liz=xbmcgui.ListItem(name, iconImage=iconimage, thumbnailImage=iconimage)
 	liz.setInfo(type='Video', infoLabels={'Title': name})
 
-	if mode == 2 or mode == 100 or mode==101 or mode==102 or mode==103 or mode==104: #playable, not browser call, needs decoded to be playable or rtmp to be obtained
+	if mode == 2 or mode == 100 or mode==101 or mode==102 or mode==103 or mode==104 or mode==105: #playable, not browser call, needs decoded to be playable or rtmp to be obtained
 		liz.setProperty("IsPlayable", "true")
 		liz.setPath(url)
 		ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False) #Playable
@@ -136,6 +137,7 @@ def browse_channels(url,page): #BROWSES ALL PROVIDERS
 	add_dir("Zoptv.com", 'zoptv', 4, "http://www.zoptv.com/images/logo.png", 'zoptv' , 0)
 	add_dir("Cineestrenostv.tv", 'cineestrenos', 4, "http://i.imgur.com/z3CINCU.jpg", 'cineestrenos' , 0)
 	add_dir("Cricfree.tv", 'cricfree', 4, "http://cricfree.tv/images/logosimg.png", 'cricfree' , 0)
+	add_dir("Live9.net", 'live9', 4, "", 'live9' , 0)
 	add_dir("Vipgoal.net", 'vigoal', 4, "http://vipgoal.net/VIPgoal/img/logo.png", 'vigoal' , 0) #this page was down, TODO: it will be replaced with the new version of this page: verliga.net
 
 def browse_channel(url,page,provider): #MAIN TREE BROWSER IS HERE!
@@ -270,6 +272,21 @@ def browse_channel(url,page,provider): #MAIN TREE BROWSER IS HERE!
 			else:
 				image = icon
 			add_dir(title,link,mode,image,"zoptv",link)
+	elif provider == 'live9':
+		jsonChannels = Live9net.getChannels(page)
+		i = 0
+		for item in jsonChannels:
+			title = item["title"]
+			link = item["link"]
+			#if item.has_key("permalink"):
+			mode = 105 #next step returns a final link
+			#else:
+			#	mode = 4 #continue browsing
+			if item.has_key("thumbnail"):
+				image = item["thumbnail"]
+			else:
+				image = icon
+			add_dir(title,link,mode,image,"live9",link)
 	logger.info(provider)
 
 def open_channel(url,page,provider=""):
@@ -344,6 +361,10 @@ def init():
 		open(channel[0]["link"],page) #same that 2, but reserved for rtmp
 	elif mode == 104:
 		channel = Zoptvcom.getChannels(page)
+		logger.info("found link: "+channel[0]["link"]+", launching...")
+		open(channel[0]["link"],page) #same that 2, but reserved for rtmp
+	elif mode == 105:
+		channel = Live9net.getChannels(page)
 		logger.info("found link: "+channel[0]["link"]+", launching...")
 		open(channel[0]["link"],page) #same that 2, but reserved for rtmp
 	xbmcplugin.endOfDirectory(int(sys.argv[1]))

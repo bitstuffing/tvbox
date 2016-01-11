054d392ee456b293f1b08a6ed748b4d6d1381ef9
bitstuffing <eduardo.higuero@hotmail.com>
2016-01-11 15:02:08 +0100

    Improves in revision, added more options for customers and updated a providers (settings parts enable the new method)

--- providers/cinestrenostv.py
+++ providers/cinestrenostv.py
@@ -2,6 +2,8 @@
 
 import httplib
 import urllib
+import os,sys
+import xbmcplugin
 from core.decoder import Decoder
 from core import logger
 from core.downloader import Downloader
@@ -14,8 +16,12 @@ class Cineestrenostv(Downloader):
     def getChannels(page):
         print "starting with page from cineestrenos section: "+page
         if str(page) == '0':
-            page = Cineestrenostv.MAIN_URL+'/carrusel/tv.html'
-            #page = Cineestrenostv.MAIN_URL
+            tradicionalChannels = xbmcplugin.getSetting(int(sys.argv[1]), "extended_channels")
+            logger.info("using carrusel: "+str(tradicionalChannels))
+            if tradicionalChannels=="false":
+                page = Cineestrenostv.MAIN_URL+'/carrusel/tv.html'
+            else:
+                page = Cineestrenostv.MAIN_URL
         html = Cineestrenostv.getContentFromUrl(page,"","",Cineestrenostv.MAIN_URL)
         x = []
         logger.info("page is: "+page)
@@ -51,12 +57,14 @@ class Cineestrenostv(Downloader):
                     element["title"] = Decoder.extract('" title="','" target',fieldHtml)
                     if element["title"].find('"')>-1:
                         element["title"] = element["title"][0:element["title"].find('"')]
-                    if element["title"].find(" online en directo")>-1:
+                    if element["title"].find(" online")>-1:
                         element["title"] = element["title"][0:element["title"].find(" online")]
                     if element["title"].find(" Online")>-1:
                         element["title"] = element["title"][0:element["title"].find(" Online")]
                     if element["title"].find(" en directo")>-1:
                         element["title"] = element["title"][0:element["title"].find(" en directo")]
+
+                    element["title"] = element["title"].replace("ver ","")
 
                 #element["title"] = element["title"].decode('utf-8')
                 element["thumbnail"] = Decoder.extract('<img src="','" height',fieldHtml)
@@ -64,7 +72,8 @@ class Cineestrenostv(Downloader):
                     element["thumbnail"] = element["thumbnail"][0:element["thumbnail"].find('"')]
                 if element["thumbnail"].find("http")==0:
                     logger.info("found title: "+element["title"]+", link: "+element["link"]+", thumb: "+element["thumbnail"])
-                    x.append(element)
+                    if element["thumbnail"].find("http")==0 and not(element["title"]=="1" or element["title"]=="gran hermano mexico" or element["title"]=="alx syfy" or element["title"]=="intereconomia punto pelota" or element["title"]=="cine" or element["title"].find("-LATINOAMERICA")>-1):
+                        x.append(element)
         else:
             logger.info('extracting channel from: '+page)
             x.append(Cineestrenostv.extractChannel(html,page))
@@ -74,7 +83,9 @@ class Cineestrenostv(Downloader):
     def extractChannel(html,referer):
         element = {}
         logger.info('processing html...')
-        if html.find('.php')>-1 and referer.find(".php")==-1:
+        if html.find('<iframe scrolling="no" marginwidth="0" marginheight="0" frameborder="0" width="650" height="400" src="')>-1:
+            element = Cineestrenostv.extractIframeChannel(html,referer)
+        elif html.find('.php')>-1 and referer.find(".php")==-1:
             logger.info("proccessing level 1, cookie: "+Cineestrenostv.cookie)
             iframeUrl = Decoder.extractWithRegex('http://','.php',html)
             if iframeUrl.find('"')>-1:
@@ -92,19 +103,28 @@ class Cineestrenostv(Downloader):
                 if html3.find('<iframe scrolling="no" marginwidth="0" marginheight="0" frameborder="0" width="650" height="400" src="')>-1:
                     element = Cineestrenostv.extractIframeChannel(html3,completeLink)
         elif referer.find("php")!=-1:
-            html2 = Cineestrenostv.getContentFromUrl(referer,"",Cineestrenostv.cookie,Cineestrenostv.MAIN_URL)
+            referer = referer.replace("ñ","%C3%B1")
+            html2 = Cineestrenostv.getContentFromUrl(referer,"",Cineestrenostv.cookie,referer)
             element = Cineestrenostv.extractIframeChannel(html2,referer)
         return element
 
     @staticmethod
     def extractIframeChannel(contentHtml,referer):
-        element = {}
         logger.info("proccessing level 2, cookie: "+Cineestrenostv.cookie)
+        iframeUrl2 = "dummy url"
         if contentHtml.find('<iframe scrolling="no" marginwidth="0" marginheight="0" frameborder="0" width="653" height="403" src="')>-1:
-            iframeUrl2 = Decoder.extract('<iframe scrolling="no" marginwidth="0" marginheight="0" frameborder="0" width="653" height="403" src="','"></iframe>',contentHtml) #same case with different width and height: TODO: change to regex!!
+            iframeUrl2 = Decoder.extract('<iframe scrolling="no" marginwidth="0" marginheight="0" frameborder="0" width="653" height="403" src="','"></iframe>',contentHtml).replace("ñ","%C3%B1") #same case with different width and height: TODO: change to regex!!
         elif contentHtml.find('<iframe scrolling="no" marginwidth="0" marginheight="0" frameborder="0" width="650" height="400" src="')>-1:
-            iframeUrl2 = Decoder.extract('<iframe scrolling="no" marginwidth="0" marginheight="0" frameborder="0" width="650" height="400" src="','"></iframe>',contentHtml) #normal cases, standar width and height
-        html3 = Cineestrenostv.getContentFromUrl(iframeUrl2,"","",referer)
+            iframeUrl2 = Decoder.extract('<iframe scrolling="no" marginwidth="0" marginheight="0" frameborder="0" width="650" height="400" src="','"></iframe>',contentHtml).replace("ñ","%C3%B1") #normal cases, standar width and height
+        if iframeUrl2!="dummy url":
+            html3 = Cineestrenostv.getContentFromUrl(iframeUrl2,"","",referer)
+            return Cineestrenostv.mainLogicExtractIframeChannel(html3,iframeUrl2)
+        else:
+            return Cineestrenostv.mainLogicExtractIframeChannel(contentHtml,referer)
+
+    @staticmethod
+    def mainLogicExtractIframeChannel(html3,iframeUrl2):
+        element = {}
         if html3.find('<script type="text/javascript" src="http://tv.verdirectotv.org/channel.php?file=')>-1:
             element = Cineestrenostv.extractScriptVerdirectotv(html3,iframeUrl2)
         elif html3.find("http://vercanalestv.com/tv/")>-1: #vercanalestv
@@ -136,7 +156,11 @@ class Cineestrenostv(Downloader):
             element["permalink"] = True
             element["link"] = playerUrl
         elif html3.find("<script type='text/javascript' src='http://www.embeducaster.com/static/scripts/ucaster.js'></script>")>-1: #ucaster cases
-            channel = Decoder.extract("<script type='text/javascript'> width=650, height=400, channel='","'",html3)
+            if html3.find("<script type='text/javascript'> width=650, height=400, channel='")>-1:
+                channel = Decoder.extract("<script type='text/javascript'> width=650, height=400, channel='","'",html3)
+            else:
+                channel = Decoder.extract("<script type='text/javascript'> width=","',",html3)
+                channel = channel[channel.find("channel='")+len("channel='"):]
             logger.info("ucaster channel: "+channel)
             if html3.find('<script type="text/javascript" src="http://tv.verdirectotv.org/channel.php?file=')>-1:
                 element = Cineestrenostv.extractScriptVerdirectotv(html3,iframeUrl2)
@@ -213,24 +237,103 @@ class Cineestrenostv(Downloader):
              element = Cineestrenostv.extractIframeChannel(html3,iframeUrl2)
         elif html3.find('<iframe  src="')>-1: #special case, it's not the standard iframe
             iframeUrl3 = Decoder.extract('<iframe  src="','" name="tv" id="tv" frameborder="0" height="400" scrolling="no" width="650"></iframe>',html3)
-            
+
         elif html3.find('file: "')>-1 and html3.find('.m3u8')>-1: #direct link, not needed any logic
             link = Decoder.extract('file: "','",',html3)
             logger.info("detected direct link: "+link)
             element["title"] = "Watching direct link"
             element["permalink"] = True
             element["link"] = link
-            '''
-        elif html3.find("http://tutvgratis.tv/embed/")>-1:
-            newUrl = Decoder.extractWithRegex('http://tutvgratis.tv/embed/','"',html3).replace('"',"")
-            #TODO, redo with other player supports (same algorithm)
-            '''
+        elif html3.find("http://telefivegb.com/")>-1 or html3.find("http://verlatelegratis.net")>-1:
+            element = Cineestrenostv.extractNewIframeChannel(html3,iframeUrl2)
+        elif html3.find("http://tvpor-internet.com/")>-1 or html3.find("http://www.malosolandia.com/")>-1:
+            logger.info("inside here!..")
+            newUrl = "dummy url" #dummy
+            if html3.find("http://tvpor-internet.com/")>-1:
+                logger.info("inside here1!..")
+                newUrl = Decoder.extractWithRegex('http://tvpor-internet.com/','"',html3).replace('"',"")
+            elif html3.find("http://www.malosolandia.com/")>-1:
+                logger.info("inside here2!..")
+                logger.info("using malosolandia")
+                newUrl = Decoder.extractWithRegex('http://www.malosolandia.com/','.html',html3)
+            else:
+                logger.info("nothing done!")
+            if newUrl!="dummy url":
+                logger.debug("html is: "+html3)
+                logger.info("using new url: "+newUrl)
+                html4 = Cineestrenostv.getContentFromUrl(newUrl,"",Cineestrenostv.cookie,iframeUrl2)
+                logger.info("redirecting using: "+newUrl+", html: "+html4)
+                element = Cineestrenostv.mainLogicExtractIframeChannel(html4,newUrl)
+            else:
+                print html3
+        elif html3.find("http://www.rtve.es/directo/la-2/")>-1:
+            element["title"] = "Watch streaming"
+            element["permalink"] = True
+            element["link"] = "http://hlslive.rtve.es/LA2_LV3_IPH/LA2_LV3_IPH.m3u8"
+        elif html3.find("http://www.rtve.es/directo/canal-24h/")>-1:
+            element["title"] = "Watch streaming"
+            element["permalink"] = True
+            element["link"] = "http://hlslive.rtve.es/24H_LV3_IPH/24H_LV3_IPH.m3u8"
+        elif html3.find("http://leton.tv/player.php")>-1:
+            logger.info("detected leton link...")
+            letonUrl = Decoder.extractWithRegex('http://leton.tv/player.php','"',html3).replace('"',"")
+            letonHtml = Cineestrenostv.getContentFromUrl(letonUrl,"",Cineestrenostv.cookie,iframeUrl2)
+            print letonHtml
+            playerUrl = Decoder.decodeLetonTv(letonHtml,letonUrl)
+            element["title"] = "Watch streaming"
+            element["permalink"] = True
+            element["link"] = playerUrl
+        elif html3.find('<iframe scrolling="no" marginwidth="0" marginheight="0" frameborder="0" width="650" height="400" src="')>-1:
+            logger.info("detected iframe with embed page, doing a new loop again...")
+            iframeUrl3 = Decoder.extract('<iframe scrolling="no" marginwidth="0" marginheight="0" frameborder="0" width="650" height="400" src="','"></iframe>',html3) #normal cases, standar width and height
+            if iframeUrl3!=iframeUrl2:
+                html4 = Cineestrenostv.getContentFromUrl(iframeUrl3,"","",iframeUrl2)
+                return Cineestrenostv.mainLogicExtractIframeChannel(html4,iframeUrl3)
+            else:
+                logger.info("infinite loop detected, stopped!")
         else: #tries to decode the bussinesslink, TODO, review this part
+            print html3
             playerUrl = Decoder.decodeBussinessApp(html3,iframeUrl2)
             logger.info("bussinessapp - player url is: "+playerUrl)
             element["title"] = "Watch streaming"
             element["permalink"] = True
             element["link"] = playerUrl
+        return element
+
+    @staticmethod
+    def extractNewIframeChannel(html3,iframeUrl2):
+        element = {}
+        if html3.find("http://telefivegb.com/")>-1:
+            logger.info("found telefivegb.com link, using that link to...")
+            newUrl = Decoder.extractWithRegex('http://telefivegb.com/','"',html3).replace('"',"")
+        elif html3.find("http://verlatelegratis.net")>-1:
+            logger.info("found verlatelegratis.net link, using that link to...")
+            newUrl = Decoder.extractWithRegex('http://verlatelegratis.net','"',html3).replace('"',"")
+        html4 = Cineestrenostv.getContentFromUrl(newUrl,"",Cineestrenostv.cookie,iframeUrl2)
+        if html4.find("http://www.playerhd1.pw/")>-1:
+            logger.info("found playerhd1.pw, using that link, continue...")
+            scriptUrl = Decoder.extractWithRegex("http://www.playerhd1.pw/",'"',html4).replace('"',"")
+
+            html4 = Cineestrenostv.getContentFromUrl(scriptUrl,"",Cineestrenostv.cookie,newUrl)
+
+            finalIframeUrl = Decoder.extractWithRegex('http://','%3D"',html4)
+            finalIframeUrl = finalIframeUrl[0:len(finalIframeUrl)-1]
+
+            logger.info("proccessing level 4, cookie: "+Cineestrenostv.cookie)
+
+            finalHtml = Cineestrenostv.getContentFromUrl(finalIframeUrl,"",Cineestrenostv.cookie,newUrl)
+            #print "final level5 html: "+finalHtml
+            logger.info("proccessing level 5, cookie: "+Cineestrenostv.cookie)
+            playerUrl = Decoder.decodeBussinessApp(finalHtml,finalIframeUrl)
+
+            #print "player url is: "+playerUrl
+            element["title"] = "Watch streaming"
+            element["permalink"] = True
+            element["link"] = playerUrl
+        else:
+            logger.info("possible redirect to his domains: "+html4+", try again..."+newUrl)
+            #element = Cineestrenostv.extractIframeChannel(html4,newUrl)
+            element = Cineestrenostv.extractNewIframeChannel(html4,newUrl)
         return element
 
     @staticmethod

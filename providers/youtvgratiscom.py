75315bc006de38a71881d34bb82a0e41aec341d8
bitstuffing <eduardo.higuero@hotmail.com>
2016-08-24 20:32:09 +0200

    Improves related to youtvgratis provider.

--- providers/youtvgratiscom.py
+++ providers/youtvgratiscom.py
@@ -16,7 +16,7 @@ class Youtvgratis(Downloader):
             i=0
             for line in html.split('<div class="col-xs-12 col-sm-2'):
                 if i>0:
-                    title = Decoder.extract('</a>','</div>',line)
+                    title = Decoder.extract('</a>','</div>',line).strip()
                     img = Decoder.extract(' src="', '"', line)
                     link = Decoder.extract('<a href="', '"', line)
                     element = {}
@@ -32,19 +32,9 @@ class Youtvgratis(Downloader):
             url2 = Decoder.extractWithRegex('http://youtvgratis.com/embed/','"',html).replace('"','')
             html2 = Youtvgratis.getContentFromUrl(url=url2,referer=page,cookie=Youtvgratis.cookie)
             if 'file: "' in html2:
-                listUrl = Decoder.extract('file: "','"',html2)
-                if "|" in listUrl:
-                    logger.debug("detected external list...")
-                    listUrl = listUrl[:listUrl.find("|")]
-                    newListContent = Youtvgratis.getContentFromUrl(url=listUrl)
-                    if ".m3u8" in newListContent and "http" in newListContent:
-                        listUrl = Decoder.extractWithRegex('http:',"=.m3u8",newListContent)
-                        logger.debug("new list has been updated to: "+listUrl)
-                    else:
-                        logger.debug("rejected new list, using last one valid.")
-                logger.debug("extracted m3u8: "+listUrl)
+                listUrl = Youtvgratis.extractFile(html2)
                 element = {}
-                element["link"] = listUrl
+                element["link"] = listUrl+"|"+Downloader.getHeaders(url2)
                 element["title"] = page
                 element["thumbnail"] = ""
                 x.append(element)
@@ -54,5 +44,34 @@ class Youtvgratis(Downloader):
                     newUrl = Decoder.extractWithRegex('http://cinestrenostv.tv/','"',html2).replace('"',"")
                     logger.debug("extracting")
                     x = Cineestrenostv.getChannels(newUrl)
+                elif 'http://embed.latino-webtv.com/' in html2:
+                    #search iframe
+                    iframeUrl = Decoder.extractWithRegex('http://embed.latino-webtv.com/','"',html2).replace('"',"")
+                    html3 = Youtvgratis.getContentFromUrl(url=iframeUrl,referer=url2)
+                    if "http://latino-webtv.com/embed/" in html3:
+                        iframeUrl2 = Decoder.extractWithRegex('http://latino-webtv.com/embed/','"',html3).replace('"',"")
+                        logger.debug("trying with latinoweb url: " + iframeUrl2)
+                        html4 = Youtvgratis.getContentFromUrl(url=iframeUrl2,referer=iframeUrl)
+                        if 'file: "' in html4:
+                            listUrl = Youtvgratis.extractFile(html4)+"|"+Downloader.getHeaders(iframeUrl2)
+                            element = {}
+                            element["link"] = listUrl
+                            element["title"] = page
+                            element["thumbnail"] = ""
+                            x.append(element)
         return x
 
+    @staticmethod
+    def extractFile(html2):
+        listUrl = Decoder.extract('file: "', '"', html2)
+        if "|" in listUrl:
+            logger.debug("detected external list...")
+            listUrl = listUrl[:listUrl.find("|")]
+            newListContent = Youtvgratis.getContentFromUrl(url=listUrl)
+            if ".m3u8" in newListContent and "http" in newListContent:
+                listUrl = Decoder.extractWithRegex('http:', "=.m3u8", newListContent)
+                logger.debug("new list has been updated to: " + listUrl)
+            else:
+                logger.debug("rejected new list, using last one valid.")
+        logger.debug("extracted m3u8: " + listUrl)
+        return listUrl

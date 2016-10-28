a69d8f0568adf0930a1021e4fb1c8d03cd7331c2
bit <bit.stuffing@gmail.com>
2016-10-28 22:42:14 +0200

    Added search Marked version.xml to update warn

--- providers/tvshowme.py
+++ providers/tvshowme.py
@@ -2,6 +2,9 @@
 from core.decoder import Decoder
 from core import logger
 from core.downloader import Downloader
+from core.xbmcutils import XBMCUtils
+
+import urllib
 
 class Tvshowme(Downloader):
 
@@ -16,6 +19,19 @@ class Tvshowme(Downloader):
             html = Tvshowme.getContentFromUrl(page,"",Tvshowme.cookie,"")
             table = Decoder.extract('<span class="yawp_wim_title">Latest 150 Posts</span> <ul>','</ul>',html)
             x = Tvshowme.extractElements(table)
+        elif page=="search":
+            #display keyboard, it will wait for result
+            keyboard = XBMCUtils.getKeyboard()
+            keyboard.doModal()
+            text = ""
+            if (keyboard.isConfirmed()):
+                text = keyboard.getText()
+                page = "http://www.tvshow.me/?s="+urllib.quote(text)
+            html = Tvshowme.getContentFromUrl(url=page)
+            logger.debug(html)
+            table = Decoder.extract('<div id="content"', '<h3 class="assistive-text">', html)
+            logger.debug("table is: "+table)
+            x = Tvshowme.extractLinks2(table)
         else:
             html = Tvshowme.getContentFromUrl(page, "", Tvshowme.cookie, Tvshowme.MAIN_URL)
             logger.debug(html)
@@ -26,11 +42,15 @@ class Tvshowme(Downloader):
     @staticmethod
     def extractElements(table):
         x = []
+        element = {}
+        element["title"] = "Search"
+        element["link"] = "search"
+        x.append(element)
         i=0
         for value in table.split('<li>'):
             if i>0:
                 element = {}
-                title = Decoder.extract('/">','</a>',value)
+                title = Decoder.extract('/">','</a>',value).replace("&#8211;","-")
                 link = Decoder.extract('<a href="','"',value)
                 element["title"] = title
                 element["link"] = link
@@ -46,7 +66,7 @@ class Tvshowme(Downloader):
         for value in html.split(' href='):
             if i>1:
                 element = {}
-                title = Decoder.extract('>','</a>', value)
+                title = Decoder.extract('>','</a>', value).replace("&#8211;","-")
                 link = Decoder.extract('"','"', value)
                 element["title"] = title
                 element["link"] = link
@@ -54,5 +74,21 @@ class Tvshowme(Downloader):
                 if "<img" not in title and "tvshow.me" not in link:
                     logger.debug("append: " + title + ", link: " + element["link"])
                     x.append(element)
+            i+=1
+        return x
+
+    @staticmethod
+    def extractLinks2(html):
+        x = []
+        i=0
+        for value in html.split('<h2 class="entry-title ">'):
+            if i>1:
+                logger.debug("partial html is: "+value)
+                element = {}
+                title = Decoder.extract(' title="','"', value)
+                link = Decoder.extract('href="','"', value)
+                element["title"] = title.replace("Permalink to ","").replace("&#8211;","-")
+                element["link"] = link
+                x.append(element)
             i+=1
         return x

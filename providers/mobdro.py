84cf391fe2026380fecfaf4bd9a46fe8a6e86687
harddevelop <harddevelop@gmail.com>
2016-12-07 16:29:23 +0100

    mobdro provider - not finished yet

--- providers/mobdro.py
+++ providers/mobdro.py
@@ -16,26 +16,110 @@ class Mobdro(Downloader):
     MAIN_URL = "mobdro.me"
     CHANNELS = "channels"
     API_URL = "https://api.mobdro.sx/streambot/v4/show"
+    API_URL_SEARCH = "https://api.mobdro.sx/streambot/v4/search"
     TOKEN = XBMCUtils.getSettingFromContext(int(sys.argv[1]), "mobdro_api")  #stored for premium support, implementation will change
 
     @staticmethod
     def getChannels(page):
         x = []
         logger.debug("using Mobdro...")
-        if str(page) == '0': #channels
-            response = Mobdro.channel_list(Mobdro.CHANNELS)
-            #parse results
-            results = json.loads(response)
-            for result in results:
-                url = parse_relayer(result)
-                if url is not "exception":
-                    element = {}
-                    element["link"] = url
-                    element["thumbnail"] = result["img"]
-                    element["title"] = result["name"]+ " - "+result["language"]
-                    logger.debug("appending: "+element["title"]+", url: "+url)
-                    x.append(element)
+        if str(page) == '0':
+            element = {}
+            element["title"]="Channels"
+            element["link"] = "channels"
+            x.append(element)
+            element = {}
+            element["title"] ="News"
+            element["link"] = "news"
+            x.append(element)
+            element = {}
+            element["title"] = "Shows"
+            element["link"] = "shows"
+            x.append(element)
+            element = {}
+            element["title"] = "Movies"
+            element["link"] = "movies"
+            x.append(element)
+            element = {}
+            element["title"] = "Sports"
+            element["link"] = "sports"
+            x.append(element)
+            element = {}
+            element["title"] = "Music"
+            element["link"] = "music"
+            x.append(element)
+            element = {}
+            element["title"] = "Gaming"
+            element["link"] = "gaming"
+            x.append(element)
+            element = {}
+            element["title"] = "Animals"
+            element["link"] = "animals"
+            x.append(element)
+            element = {}
+            element["title"] = "Tech"
+            element["link"] = "tech"
+            x.append(element)
+            element = {}
+            element["title"] = "Podcasts"
+            element["link"] = "podcasts"
+            x.append(element)
+            element = {}
+            element["title"] = "Spiritual"
+            element["link"] = "spiritual"
+            x.append(element)
+            element = {}
+            element["title"] = "Others"
+            element["link"] = "others"
+            x.append(element)
+            element = {}
+            element["title"] = "Search"
+            element["link"] = "search"
+            #x.append(element) #TODO
+        elif str(page) is not 'search': #action
+            logger.debug("launching action: "+page)
+            response = Mobdro.channel_list(page)
+            x = Mobdro.parse_results(response)
+        elif str(page) == "search": #search
+            logger.debug("launching action: SEARCH")
+            # display keyboard, it will wait for result
+            keyboard = XBMCUtils.getKeyboard()
+            keyboard.doModal()
+            text = ""
+            if (keyboard.isConfirmed()):
+                text = keyboard.getText()
+                response = Mobdro.search_list(text)
+                x = Mobdro.parse_results(response)
         return x
+
+    @staticmethod
+    def parse_results(response):
+        x = []
+        # parse results
+        results = json.loads(response)
+        for result in results:
+            url = parse_relayer(result)
+            if url is not "exception":
+                element = {}
+                element["link"] = url
+                element["thumbnail"] = result["img"]
+                element["title"] = result["name"] + " - " + result["language"]
+                element["finalLink"] = True
+                logger.debug("appending: " + element["title"] + ", url: " + url)
+                x.append(element)
+        return x
+
+    @staticmethod
+    def search_list(term):
+        response = "ERROR"
+        c_headers = {"User-Agent": "Mobdro/5.0", "Referer": "api.mobdro.sx"}
+        c_data = {'query':term,'parental':0,'languages':'[]','alphabetical':0,'token': Mobdro.TOKEN}
+        c_data = urllib.urlencode(c_data)
+        # Fetch channel list
+        req = urllib2.Request(Mobdro.API_URL_SEARCH, c_data, c_headers)
+        response = urllib2.urlopen(req)
+        response = response.read()
+        return response
 
     @staticmethod
     def channel_list(action):
@@ -79,7 +163,7 @@ def parse_relayer(params):
             arr = [protocol,server,app,playpath,url_base64,l]
             url = "%s://%s/%s/%s?st=%s&e=%d" #"http://%s/live/%s/%d/%s"
             url = url % tuple(arr)
-            url += "|Referer="+Mobdro.MAIN_URL+"&User-Agent=Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.100 Safari/537.36&Icy-MetaData=0"
+            url += "|"+Downloader.getHeaders(Mobdro.MAIN_URL)
         else:
             logger.debug("REJECTED: " + repr(params))
     except KeyError:

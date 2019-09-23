547d4c487682eb3c88773aa7977415c9367bafc7
bitstuffing <eduardo.higuero@hotmail.com>
2019-09-23 23:06:41 +0200

    unfinished fix for provider

--- providers/tvporinternetnet.py
+++ providers/tvporinternetnet.py
@@ -6,7 +6,7 @@ from tvboxcore.downloader import Downloader
 
 class Tvporinternetnet(Downloader):
 
-    MAIN_URL = "http://tvpor-internet.com/channels"
+    MAIN_URL = "http://tvporinternet.eshost.com.ar"
 
     @staticmethod
     def getChannels(page):
@@ -17,31 +17,32 @@ class Tvporinternetnet(Downloader):
         html = Tvporinternetnet.getContentFromUrl(page,"",Tvporinternetnet.cookie,"")
         x = []
         if page == Tvporinternetnet.MAIN_URL:
-            table = Decoder.extract('<div id="canalesdown" class="canales">',"<br>",html)
-            for fieldHtml in table.split('<li'):
-                element = {}
-                element["link"] = Decoder.extract('<a href="','"',fieldHtml)
-                element["title"] = Decoder.extract('<div style="color: #ffffff;">','<',fieldHtml)
-                element["thumbnail"] = Decoder.extract('<img src="','"',fieldHtml)
-                element["permaLink"] = True
-                logger.debug("found title: "+element["title"]+", link: "+element["link"]+", thumb: "+element["thumbnail"])
-                if "http" in element["link"]:
-                    x.append(element)
+            table = Decoder.extract('<div data-p="43.75">',"<!-- Bullet Navigator -->",html)
+            for fieldHtml in table.split('<div data-p="43.75">'):
+                if ' href=' in fieldHtml:
+                    element = {}
+                    element["link"] = Decoder.extract('<a href="','"',fieldHtml)
+                    element["title"] = Decoder.extract('/p/','-en-',element["link"])
+                    element["thumbnail"] = Decoder.extract('<img data-u="image" src="','"',fieldHtml)
+                    element["permaLink"] = True
+                    logger.debug("found title: "+element["title"]+", link: "+element["link"]+", thumb: "+element["thumbnail"])
+                    if "http" in element["link"]:
+                        x.append(element)
         else:
             x.append(Tvporinternetnet.extractChannel(html,page))
         return x
 
     @staticmethod
-    def extractChannel(html,page="http://tvpor-internet.com"):
+    def extractChannel(html,page=MAIN_URL):
         element = {}
-        if "<script type='text/javascript'>" in html: #old part
-            script = Decoder.extract("<script type='text/javascript'>","</",html)
-            cosa = Decoder.extract("cosa = '","'",script)
-            id = Decoder.extract("id='", "'", script)
-            scriptUrl = "http://tvpor-internet.com/player/"+cosa+"/"+id
-            html2 = Tvporinternetnet.getContentFromUrl(scriptUrl)
-            lastUrl = Decoder.extract('<source src="','"',html2)
-            logger.debug("decoded link is: "+lastUrl)
-            element["title"] = page
-            element["link"] = lastUrl
+        if '<iframe src="' in html:
+            iframe = Decoder.extract('<iframe src="','"',html)
+            logger.debug("iframe url is %s"%iframe)
+            html2 = Tvporinternetnet.getContentFromUrl(url=iframe,referer=page)
+            logger.debug("html: %s"%html2)
+            if 'source: "' in html2:
+                source = Decoder.extract('source: "','"',html2)
+                logger.debug("found link: %s"%source)
+                element["link"] = source+"|User-Agent=Mozilla%2F5.0+%28X11%3B+Linux+x86_64%3B+rv%3A68.0%29+Gecko%2F20100101+Firefox%2F68.0&amp;Referer="+urllib.quote_plus(iframe)
+                element["title"] = page
         return element
